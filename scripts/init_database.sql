c9f7a28995491cd4c810ecac111cacb4784bcbb9
Ayadi Aziz <107238076+Aziz-Ayadi@users.noreply.github.com>
2025-11-16 11:20:33 +0100

    Create init_database.sql for DataWarehouse setup
    This script creates a new database named 'DataWarehouse' and sets up three schemas: 'bronze', 'silver', and 'gold'. It drops the existing database if it exists, so caution is advised.

---
+++ scripts/init_database.sql
@@ -0,0 +1,55 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+/*
+  This does two important things:
+
+  a) SINGLE_USER mode
+
+  Only one user (you) can be connected to that database.
+  
+  b) WITH ROLLBACK IMMEDIATE
+  
+  Any other users or running queries are instantly disconnected, and their transactions are rolled back.
+  
+  This is necessary because SQL Server will not drop a database if other sessions are using it.
+*/
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
